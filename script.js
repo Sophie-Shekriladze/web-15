8491d32f2df98e09cfa4e5b29fa362384eb91736
Sophie Shekriladze <55558347+Sophie-Shekriladze@users.noreply.github.com>
2023-02-21 23:13:11 +0400

    add burger bar func

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-document.getElementById('clickMe').addEventListener('click', function() {
+document.getElementById('clickMe').addEventListener('click', function(){
    alert('Coming Soon')
 })
+// burger
+let navigationElement = document.getElementById("navBar");
+let burgerElement = document.getElementById("burgerBar");
+
+burgerElement.addEventListener("click", function () {
+    navigationElement.classList.toggle("activeNav"); 
+});
